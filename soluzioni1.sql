791978d202aae375d9033907776c5025ffaf2dc9
Ivan Caldarella <gamein3x@gmail.com>
2026-06-04 18:06:41 +0200

    Esercizio completato

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -36,6 +36,16 @@ WHERE hour > "14:00:00" AND date > "2020-06-20";
 
 /* Milestone 6 - Selezionare tutti i corsi di laurea magistrale (38) */
 
+SELECT *
+FROM degrees
+WHERE level = "magistrale";
+
 /* Milestone 7 - Da quanti dipartimenti è composta l'università? (12) */
 
+SELECT * FROM departments;
+
 /* Milestone 8 - Quanti sono gli insegnanti che non hanno un numero di telefono? (50) */
+
+SELECT *
+FROM teachers
+WHERE phone IS NULL;
