7db9e90d62dea541ea758bba547403df30fe6e95
Ivan Caldarella <gamein3x@gmail.com>
2026-06-04 17:52:44 +0200

    Milestone 4 e 5

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -23,8 +23,16 @@ WHERE date_of_birth < DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 /* Milestone 4 - Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 laurea (286) */
 
+SELECT *
+FROM courses
+WHERE year = 1 AND period = "I semestre";
+
 /* Milestone 5 -  Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020 (21) */
+
+SELECT *
+FROM exams
+WHERE hour > "14:00:00" AND date > "2020-06-20";
 
 /* Milestone 6 - Selezionare tutti i corsi di laurea magistrale (38) */
 
