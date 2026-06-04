8f1f36d698e81fc11920ec803c38f75e8d79ca4d
Ivan Caldarella <gamein3x@gmail.com>
2026-06-04 17:36:35 +0200

    Milestone 2 e 3

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -7,3 +7,15 @@ WHERE date_of_birth BETWEEN '1990-01-01' AND '1990-12-31';
 SELECT *
 FROM students
 WHERE date_of_birth LIKE '1990-%';
+
+/* Milestone 2 */
+
+SELECT *
+FROM courses
+WHERE cfu > 10;
+
+/* Milestone 3 */
+
+SELECT *
+FROM students
+WHERE date_of_birth < DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
