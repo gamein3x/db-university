d01a014021d64e0c219d919ecedf3ba4c9b08f7e
Ivan Caldarella <gamein3x@gmail.com>
2026-06-08 09:40:51 +0200

    Milestone 2, 3 e 4 (GROUP BY)

--- soluzioni_GROUP_BY.sql
+++ soluzioni_GROUP_BY.sql
@@ -5,3 +5,17 @@
 SELECT YEAR(enrolment_date) AS enrolment_year, COUNT(*) AS total_students
 FROM students
 GROUP BY YEAR(enrolment_date);
+
+/* Milestone 2 * - Contare gli insegnanti che hanno l'ufficio nello stesso edificio */
+
+SELECT office_address as Palazzo, COUNT(*) AS "Condiviso da n. Professori"
+FROM teachers
+GROUP BY office_address;
+
+/* Milestone 3 * -  Calcolare la media dei voti di ogni appello d'esame */
+
+SELECT exam_id, AVG(vote) AS average_grade
+FROM exam_student
+GROUP BY exam_id;
+
+/* Milestone 4*  */
